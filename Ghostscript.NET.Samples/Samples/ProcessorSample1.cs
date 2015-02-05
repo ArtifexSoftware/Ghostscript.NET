515e6b5b4a8edf8cdda94d294db3544bce4c2d87
jhabjan <habjan@gmail.com>
2015-02-05 12:59:06 +0100

    new samples added

---
+++ Ghostscript.NET.Samples/Samples/ProcessorSample1.cs
@@ -0,0 +1,72 @@
+﻿//
+// ProcessorSample.cs
+// This file is part of Ghostscript.NET.Samples project
+//
+// Author: Josip Habjan (habjan@gmail.com, http://www.linkedin.com/in/habjan) 
+// Copyright (c) 2013-2015 by Josip Habjan. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using Ghostscript.NET.Processor;
+
+namespace Ghostscript.NET.Samples
+{
+    public class ProcessorSample1 : ISample
+    {
+        public void Start()
+        {
+            string inputFile = @"E:\gss_test\test.pdf";
+            string outputFile = @"E:\gss_test\output\page-%03d.png";
+
+            int pageFrom = 1;
+            int pageTo = 50;
+
+            using (GhostscriptProcessor ghostscript = new GhostscriptProcessor())
+            {
+                ghostscript.Processing += new GhostscriptProcessorProcessingEventHandler(ghostscript_Processing);
+
+                List<string> switches = new List<string>();
+                switches.Add("-empty");
+                switches.Add("-dSAFER");
+                switches.Add("-dBATCH");
+                switches.Add("-dNOPAUSE");
+                switches.Add("-dNOPROMPT");
+                switches.Add("-dFirstPage=" + pageFrom.ToString());
+                switches.Add("-dLastPage=" + pageTo.ToString());
+                switches.Add("-sDEVICE=png16m");
+                switches.Add("-r96");
+                switches.Add("-dTextAlphaBits=4");
+                switches.Add("-dGraphicsAlphaBits=4");
+                switches.Add(@"-sOutputFile=" + outputFile);
+                switches.Add(@"-f");
+                switches.Add(inputFile);
+
+                ghostscript.Process(switches.ToArray());
+            }
+        }
+
+        void ghostscript_Processing(object sender, GhostscriptProcessorProcessingEventArgs e)
+        {
+            Console.WriteLine(e.CurrentPage.ToString() + " / " + e.CurrentPage.ToString());
+        }
+    }
+}
---
+++ Ghostscript.NET.Samples/Samples/RasterizerSample1.cs
@@ -0,0 +1,73 @@
+﻿//
+// RasterizerSample.cs
+// This file is part of Ghostscript.NET.Samples project
+//
+// Author: Josip Habjan (habjan@gmail.com, http://www.linkedin.com/in/habjan) 
+// Copyright (c) 2013-2015 by Josip Habjan. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+// required Ghostscript.NET namespaces
+using Ghostscript.NET;
+using Ghostscript.NET.Rasterizer;
+
+namespace Ghostscript.NET.Samples
+{
+
+    /// <summary>
+    /// GhostscriptRasterizer allows you to rasterize pdf and postscript files into the 
+    /// memory. If you want Ghostscript to store files on the disk use GhostscriptProcessor
+    /// or one of the GhostscriptDevices (GhostscriptPngDevice, GhostscriptJpgDevice).
+    /// </summary>
+    public class RasterizerSample1 : ISample
+    {
+        private GhostscriptVersionInfo _lastInstalledVersion = null;
+
+        public void Start()
+        {
+            int desired_x_dpi = 96;
+            int desired_y_dpi = 96;
+
+            string inputPdfPath = @"E:\gss_test\test.pdf";
+            string outputPath = @"E:\gss_test\output\";
+
+            using (GhostscriptRasterizer rasterizer = new GhostscriptRasterizer())
+            {
+                rasterizer.Open(inputPdfPath);
+
+                for (int pageNumber = 1; pageNumber <= rasterizer.PageCount; pageNumber++)
+                {
+                    string pageFilePath = Path.Combine(outputPath, "Page-" + pageNumber.ToString() + ".png");
+
+                    Image img = rasterizer.GetPage(desired_x_dpi, desired_y_dpi, pageNumber);
+                    img.Save(pageFilePath, ImageFormat.Png);
+
+                    Console.WriteLine(pageFilePath);
+                }
+            }
+        }
+    }
+}
+
