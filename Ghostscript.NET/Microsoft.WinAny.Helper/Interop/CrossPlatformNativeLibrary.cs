9793aaa81e841f0432767e4c1cd7767eb9653447
MaksymTkachuk <maksym.tkachuk@artifex.com>
2025-10-28 17:40:21 +0200

    updated the header comment on new files

--- Ghostscript.NET/Microsoft.WinAny.Helper/Interop/CrossPlatformNativeLibrary.cs
+++ Ghostscript.NET/Microsoft.WinAny.Helper/Interop/CrossPlatformNativeLibrary.cs
@@ -1,28 +1,25 @@
+// Copyright (C) 2025 Artifex Software, Inc.
 //
-// CrossPlatformNativeLibrary.cs
-// This file is part of Microsoft.WinAny.Helper library
+// This file is part of Ghostscript.NET.
 //
-// Author: Josip Habjan (habjan@gmail.com, http://www.linkedin.com/in/habjan) 
-// Copyright (c) 2013-2016 by Josip Habjan. All rights reserved.
+// Ghostscript.NET is free software: you can redistribute it and/or modify it 
+// under the terms of the GNU Affero General Public License as published by the 
+// Free Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
 //
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
+// Ghostscript.NET is distributed in the hope that it will be useful, but WITHOUT 
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+// details.
 //
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
+// You should have received a copy of the GNU Affero General Public License
+// along with Ghostscript.NET. If not, see 
+// <https://www.gnu.org/licenses/agpl-3.0.en.html>
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// Alternative licensing terms are available from the licensor.
+// For commercial licensing, see <https://www.artifex.com/> or contact
+// Artifex Software, Inc., 39 Mesa Street, Suite 108A, San Francisco,
+// CA 94129, USA, for further information.
 
 using System;
 using System.Runtime.InteropServices;
